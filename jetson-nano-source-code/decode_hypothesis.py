88c6cb8964cf19d33e3a96d1b5ba9c5a885d4e88
Jordan Micah Bennett <jordanmicahbennett@gmail.com>
2019-11-21 18:01:05 -0500

    Add files via upload

---
+++ jetson-nano-source-code/decode_hypothesis.py
@@ -0,0 +1,66 @@
+###################
+#File created by Jordan Bennett
+#Part B/3b. Ready-made decoder to decode pothole neural network output from 5D array, to simple outcome.
+#Note by Jordan: I took this from original desktop-version pothole code. I took relevant params to decode_netout from the config file of said repository.
+#The only param that is dynamic here is the neural network hypothesis/output="netout".
+###################
+import numpy as np
+from utils import _sigmoid
+from utils import _softmax
+
+anchors__=[0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
+obj_threshold__=0.3
+nms_threshold__=0.3
+nb_class__=1 #jordan_addition: where 1 is equivalent to the number of labels of this neural network, i.e. only "pothole"
+
+  
+def decode_hypothesis(netout, anchors=anchors__, nb_class=nb_class__, obj_threshold=obj_threshold__, nms_threshold=nms_threshold__):
+    grid_h, grid_w, nb_box = netout.shape[:3]
+
+    boxes = []
+    
+    # decode the output by the network
+    netout[..., 4]  = _sigmoid(netout[..., 4])
+    netout[..., 5:] = netout[..., 4][..., np.newaxis] * _softmax(netout[..., 5:])
+    netout[..., 5:] *= netout[..., 5:] > obj_threshold
+    
+    for row in range(grid_h):
+        for col in range(grid_w):
+            for b in range(nb_box):
+                # from 4th element onwards are confidence and class classes
+                classes = netout[row,col,b,5:]
+                
+                if np.sum(classes) > 0:
+                    # first 4 elements are x, y, w, and h
+                    x, y, w, h = netout[row,col,b,:4]
+
+                    x = (col + _sigmoid(x)) / grid_w # center position, unit: image width
+                    y = (row + _sigmoid(y)) / grid_h # center position, unit: image height
+                    w = anchors[2 * b + 0] * np.exp(w) / grid_w # unit: image width
+                    h = anchors[2 * b + 1] * np.exp(h) / grid_h # unit: image height
+                    confidence = netout[row,col,b,4]
+                    
+                    box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, confidence, classes)
+                    
+                    boxes.append(box)
+
+    # suppress non-maximal boxes
+    for c in range(nb_class):
+        sorted_indices = list(reversed(np.argsort([box.classes[c] for box in boxes])))
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+            
+            if boxes[index_i].classes[c] == 0: 
+                continue
+            else:
+                for j in range(i+1, len(sorted_indices)):
+                    index_j = sorted_indices[j]
+                    
+                    if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_threshold:
+                        boxes[index_j].classes[c] = 0
+                        
+    # remove the boxes which are less likely than a obj_threshold
+    boxes = [box for box in boxes if box.get_score() > obj_threshold]
+    
+    return boxes 
---
+++ jetson-nano-source-code/load_trt_graph.py
@@ -0,0 +1,51 @@
+###################
+#File created by Jordan Bennett
+#Part B/3a. Load tensor RT fp16 graph
+#This is not expected to work on windows.
+#Expected to work on google colab or jetson nano like platform.
+###################
+import tensorflow as tf
+
+#jordan_declaration: input and output names taken from frozen graph generation process
+#this is to avoid re-running frozen graph generation on jetson nano, instead okay to use generatred frozen graph file on storage.
+input_names =  ['input_1', 'input_2']
+output_names_ = ['lambda_2/Identity']
+
+def get_frozen_graph(graph_file):
+    """Read Frozen Graph file from disk."""
+    with tf.gfile.FastGFile(graph_file, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    return graph_def
+
+
+#trt_graph = get_frozen_graph('pothole_model_tensor_rt_format/trt_pothole_graph.pb') #reads from colab directory
+trt_graph = get_frozen_graph('trt_pothole_graph.pb') #reads from google drive
+
+# Create session and load graph
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+tf_sess = tf.Session(config=tf_config)
+tf.import_graph_def(trt_graph, name='')
+
+
+# Get graph input size
+for node in trt_graph.node:
+    if 'input_' in node.name:
+        size = node.attr['shape'].shape
+        image_size = [size.dim[i].size for i in range(1, 4)]
+        break
+print("image_size: {}".format(image_size))
+
+
+# input and output tensor names.
+input_tensor_name = input_names[0] + ":0"
+output_tensor_name = output_names_[0] + ":0"
+
+print("input_tensor_name: {}\noutput_tensor_name: {}".format(
+    input_tensor_name, output_tensor_name))
+
+with tf.Session() as sess: #jordan_node added these two lines to resolve FailedPreconditionError, that happens in Part B/4 prediciton on runtime.
+  tf_sess.run(tf.global_variables_initializer())
+
+output_tensor = tf_sess.graph.get_tensor_by_name(output_tensor_name)
